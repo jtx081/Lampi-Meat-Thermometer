56c8ea901e7a31dbc690bb582e81c2f32741a287
Wonyoung <wxc366@case.edu>
2022-04-11 22:04:19 +0100

    working kivy display with probe reading

--- test_subscribe.py
+++ test_subscribe.py
@@ -1,9 +1,21 @@
-from paho.mqtt.client import Client
+import paho.mqtt.client as MQTT
+import json
 
 c = MQTT.Client()
 c.enable_logger()
 c.connect('localhost', port=1883, keepalive=60)
-c.loop_start()
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    client.subscribe('test/temperature')
 
 def message_received(client, userdata, message):
   print("Topic: '{}' Payload: '{}'".format(message.topic, message.payload))
+  new_temp = round(json.loads(message.payload.decode('utf-8')), 2)
+  print('new temp: ' + str(new_temp))
+
+c.on_connect = on_connect
+c.on_message = message_received
+
+c.loop_forever()
