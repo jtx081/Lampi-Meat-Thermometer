58bf70548a23d6f1916e06cc7a40c79f5fb4ece8
joanna <joannatyan12@gmail.com>
2022-04-11 21:09:13 -0400

    ios bluetooth

---
+++ ios/meat-thermometer/meat-thermometer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  meat-thermometer
+//
+//  Created by joanna tyan on 4/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/meat-thermometer/meat-thermometer/MeatThermometer.swift
@@ -0,0 +1,17 @@
+//
+//  meat_thermometerApp.swift
+//  meat-thermometer
+//
+//  Created by joanna tyan on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MeatThermometer: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
